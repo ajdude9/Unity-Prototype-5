9bbb570bb67cc43f5f64a5a2a7f507aba9aef82a
ajdude9 <adamtheteeth@hotmail.com>
2024-09-19 13:31:10 +0100

    Unity Challenge 5 changes; added and debugged project files; custom game over text.

---
+++ Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+using System.Diagnostics;
+using Random = UnityEngine.Random;
+
+public class GameManagerX : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI timeUpText;
+    public TextMeshProUGUI missFoodText;
+    public GameObject titleScreen;
+    public Button restartButton; 
+
+    public List<GameObject> targetPrefabs;
+
+    private int score;
+    private float spawnRate = 1.5f;
+    public bool isGameActive;
+
+    private float spaceBetweenSquares = 2.5f; 
+    private float minValueX = -3.75f; //  x value of the center of the left-most square
+    private float minValueY = -3.75f; //  y value of the center of the bottom-most square
+
+    private float timeRemaining = 60;
+    
+    // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
+    public void StartGame(int difficulty)
+    {
+        spawnRate /= difficulty;
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        StartCoroutine(countDown());
+        score = 0;
+        UpdateScore(0);
+        titleScreen.SetActive(false);
+        timerText.text = "Time Left: " + timeRemaining + "s";
+    }
+
+    // While game is active spawn a random target
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targetPrefabs.Count);
+
+            if (isGameActive)
+            {
+                Instantiate(targetPrefabs[index], RandomSpawnPosition(), targetPrefabs[index].transform.rotation);
+            }
+            
+        }
+    }
+
+    IEnumerator countDown()
+    {
+        while(isGameActive)
+        {
+
+            yield return new WaitForSeconds(1);      
+            if(timeRemaining > 0)
+            {
+                UpdateTime(1);
+            }
+            else
+            {
+                GameOver("time");
+            }
+            
+        }
+    }
+
+    // Generate a random spawn position based on a random index from 0 to 3
+    Vector3 RandomSpawnPosition()
+    {
+        float spawnPosX = minValueX + (RandomSquareIndex() * spaceBetweenSquares);
+        float spawnPosY = minValueY + (RandomSquareIndex() * spaceBetweenSquares);
+
+        Vector3 spawnPosition = new Vector3(spawnPosX, spawnPosY, 0);
+        return spawnPosition;
+
+    }
+
+    // Generates random square index from 0 to 3, which determines which square the target will appear in
+    int RandomSquareIndex()
+    {
+        return Random.Range(0, 4);
+    }
+
+    // Update score with value from target clicked
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void UpdateTime(int timeToChange)
+    {
+        timeRemaining -= timeToChange;
+        timerText.text = "Time Left: " + timeRemaining + "s";
+    }
+
+    // Stop game, bring up game over text and restart button
+    public void GameOver(String type)
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+        switch(type)
+        {
+            case "time":
+                timeUpText.gameObject.SetActive(true);
+            break;
+            case "miss":
+                missFoodText.gameObject.SetActive(true);
+            break;
+        }
+    }
+
+    // Restart game by reloading the scene
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
